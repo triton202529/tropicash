e8bab9d2085062d1a7ca0de32b80205b1b2bef78
Akim <akimwallacee21@gmail.com>
2026-05-03 18:58:46 -0400

    Add Tropicash PWA manifest and app icons

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,13 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#003B73" />
+        <meta name="application-name" content="Tropicash" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="Tropicash" />
+        <link rel="apple-touch-icon" href="/icons/icon-192.png" />
       </Head>
       <body className="antialiased">
         <Main />
